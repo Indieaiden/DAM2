b95817ee1cdc448463e26ddde286dab541b67297
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-11-14 13:16:40 +0100

    add ejercicio9 tema 1

---
+++ Jorge/Interfaces/src/module-info.java
@@ -0,0 +1,10 @@
+module unidad1 {
+	exports DialogosModales;
+	exports CreacionInterfacesI;
+	exports CreacionInterfacesII;
+	exports Menus;
+	exports JOptionPane;
+
+	requires java.desktop;
+	requires java.logging;
+}
