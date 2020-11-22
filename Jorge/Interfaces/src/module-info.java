602ba0096560124317193ad33a579305c6c009ce
Jorge Aiden <jorgeaiden.martin@gmail.com>
2020-11-22 13:50:54 +0100

    EjercicioTabla hechas primera y segunda boton

--- Jorge/Interfaces/src/module-info.java
+++ Jorge/Interfaces/src/module-info.java
@@ -1,10 +1,4 @@
 module unidad1 {
-	exports DialogosModales;
-	exports CreacionInterfacesI;
-	exports CreacionInterfacesII;
-	exports Menus;
-	exports JOptionPane;
-
 	requires java.desktop;
 	requires java.logging;
 }
